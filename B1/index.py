0661fd9e22912977d1babc89dfdf3e55789d1886
KietIT-bip-bip <dangquockiet1501.tt@gmail.com>
2025-11-30 14:25:02 +0700

    first commit

---
+++ B1/index.py
@@ -0,0 +1,6 @@
+# nhập thong tin cơ bản -> in ra màn hình
+name = input("Bạn tên gì?")
+tuổi = input("Bạn mấy tuổi?")
+sothich = input(" Bạn có sở thích gì?")
+# in ra màn hình
+print(f""" Xin chào bạn {name}, tuổi của bạn là {tuổi}, có sở thích {sothich}""")
