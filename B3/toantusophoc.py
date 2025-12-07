4696150865341bd90a0202f0dbc0fa5612868028
KietIT-bip-bip <dangquockiet1501.tt@gmail.com>
2025-12-07 15:57:53 +0700

    toanlogicblablabla

---
+++ B3/toantusophoc.py
@@ -0,0 +1,18 @@
+# ======================================
+# + - * / 
+# / : trả về số thập phân
+print(5 / 2)# 2.5
+
+# * . x có thể dung cho string " str"*n ( lặp lại str số lần n)
+print("ha * 3")#hahaha
+
+# +: có thể nối chuỗi
+print("hello" + " " + "world")#hello wolrd
+
+#================================
+# //: phép chia lấy phần nguyên => trả về số nguyên
+print(5 // 2)  # 2
+# %: phép chia lấy phần dư => trả về số nguyên
+print(5 % 2)  # 1
+# **: lũy thừa 
+print(4**2)  # 16
